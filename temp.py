2154fe7736c016bb5cc670d1d71d362c0b8848a5
DeniseMarrero <61274287+DeniseMarrero@users.noreply.github.com>
2020-02-28 17:46:35 +0100

    Add files via upload

--- temp.py
+++ temp.py
@@ -4,20 +4,23 @@ import numpy as np
 import sys
 import _thread
 
+#Getting info of com ports available
 def get_serialInfo():
     portData = serial.tools.list_ports.comports()
     for j in portData:
         print(j)
-
+#------try except
+        
 # Opening serial port  
 def OpenSerial():
-        try:
-            ser = serial.Serial('COM3', baudrate = 115200) 
-            print('Connected to ' + ser.name)
-        except Exception as error:
-            print('Exception: Opening serial port: ' + str(error))
-            ser.close()
-            ser.open()
+    try:
+        ser = serial.Serial('COM3', baudrate = 115200)
+        print('Connected to ' + ser.name)
+        return ser
+    except Exception as error:
+        print('Exception: Opening serial port: ' + str(error))
+        ser.close()
+        ser.open()
             
 # Closing serial port  
 def CloseSerial():
@@ -26,11 +29,10 @@ def CloseSerial():
 
 def RetrieveData():
     rawdata = ser.read_all()
-    print(rawdata) 
-    for s in rawdata:
-        print(s)
-        data.append(s)
-        
+    print(rawdata)
+    data = [ord(c) for c in rawdata.decode()]
+    print (data)
+    
 OpenSerial()
 
 RetrieveData()
@@ -39,9 +41,17 @@ CloseSerial()
         
 get_serialInfo()       
 
-     
 
-        
+#from ASCII values to characters
+data =[1, 32, 55, 2, 87, 69, 76, 67, 79, 77, 69, 3, 53, 65, 4, 1, 49, 52, 2, 90, 77, 101, 116, 101, 114, 52, 119, 32, 118, 32, 52, 46,
+ 50, 32, 67, 79, 82, 66, 73, 3, 55, 57, 4]
+characters = [chr(ascii) for ascii in data]
+''.join(characters)
+
+#from characters to ASCII values
+s = '\x01 7\x02WELCOME\x035A\x04\x0114\x02ZMeter4w v 4.2 CORBI\x0379\x04'
+ASCII = [ord(c) for c in s]
+
 def ReceiveData (str data):
         for i in range (0, len(data)):
             
@@ -61,26 +71,19 @@ else:
 if 4 in rawdata:print ('First line \n Second line')
 
 
-#from ASCII values to characters
-data =[1, 32, 55, 2, 87, 69, 76, 67, 79, 77, 69, 3, 53, 65, 4, 1, 49, 52, 2, 90, 77, 101, 116, 101, 114, 52, 119, 32, 118, 32, 52, 46,
- 50, 32, 67, 79, 82, 66, 73, 3, 55, 57, 4]
-characters = [chr(ascii) for ascii in data]
-''.join(characters)
+# to get how many 4 there are in data string / generator expression
+data_pos = [i for i, n in enumerate(data) if n ==4]
+data_num = len([i for i, n in enumerate(data) if n ==4])
 
-#from characters to ASCII values
-s = '\x01 7\x02WELCOME\x035A\x04\x0114\x02ZMeter4w v 4.2 CORBI\x0379\x04'
-ASCII = [ord(c) for c in s]
+end_trans = None
+for i in range(0,len(data)):
+    if i == 4:
+        print(i)
+        end_trans = i +1
+        break
+else:
+    print('No end of transmission')
 
-
-            
-def OpenSerial2():
-    ser = serial.Serial('COM3', baudrate = 115200, timeout = None)
-    if ser.is_open == False:
-        print('Not connected')
-        ser.close()
-    else:
-        print(ser.name +' Connected')
-        ser.open()
         
             
 

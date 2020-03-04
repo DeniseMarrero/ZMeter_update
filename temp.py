6974536239db64fb8c0c694febf1341a0aef90fd
DeniseMarrero <61274287+DeniseMarrero@users.noreply.github.com>
2020-03-04 17:43:41 +0100

    Add files via upload

--- temp.py
+++ temp.py
@@ -4,83 +4,123 @@ import numpy as np
 import sys
 import _thread
 
+#Getting info of com ports available
 def get_serialInfo():
     portData = serial.tools.list_ports.comports()
     for j in portData:
         print(j)
-
-# Opening serial port  
+#------try except
+        
+# Opening serial port
 def OpenSerial():
-        try:
-            ser = serial.Serial('COM3', baudrate = 115200) 
-            print('Connected to ' + ser.name)
-        except Exception as error:
-            print('Exception: Opening serial port: ' + str(error))
-            ser.close()
-            ser.open()
-            
+    try:
+        ser = serial.Serial('COM5', baudrate = 115200)
+        ser.open()
+        print('Connected to ' + ser.name)
+    except Exception as error:
+        print('Exception: Opening serial port: ' + str(error))
+
 # Closing serial port  
 def CloseSerial():
+    if ser.is_open == True:
         ser.close()
         print ('Closing ' + ser.name)
+    else: 
+        print(ser.name + ' already closed')
 
-def RetrieveData():
+def ReadSerial():
     rawdata = ser.read_all()
-    print(rawdata) 
-    for s in rawdata:
-        print(s)
-        data.append(s)
-        
-OpenSerial()
+    data = np.array([ord(c) for c in rawdata.decode()])
+    print (rawdata)
+    print (data)
+    return data
 
-RetrieveData()
+def CheckSum(data):
+    temporal = 0
+    for d in data:
+        temporal = temporal ^ d
+        return temporal
+    
+data1 = '\x01 7\x02WELCOME\x035A\x04\x0114\x02ZMeter4w v 4.2 CORBI\x0379\x04'
+
+def ReceiveData(data):
+    tempLine = 0
+    tempVal = 0
+    tempLen = 0
+    state = 0
+    for i in range(len(data)):
+        if state == 0:
+            if data[0] == 1:
+                state += 1
+                tempLine = 0
+                tempVal = 0
+                print('0.0')
+        elif state == 1:
+            if (data[0] != 32):
+                tempVal = tempVal + data
+                print('0.1')
+            state += 1
+        elif state == 2:
+            tempVal = tempVal + data
+            try:
+                tempLen = tempVal
+                state += 1
+                tempVal = 0
+                print('1.0')
+            except:
+                state = 0
+        elif state == 3:
+            if data[0] == 2:
+                state += 1
+                print('2.0')
+            else:
+                state = 0
+        elif state == 4:
+            if data[0] == 3:
+                state += 1
+                print('3.0')
+            else:
+                tempLine = tempLine + data
+            if len(tempLine) != tempLen:
+                state = 0
+        elif state == 5:
+            if len(tempLine) != tempLen:
+                state = 0
+                print('4.0')
+            elif data != 32:
+                tempVal = tempVal + data
+            state += 1
+        elif state == 6:
+            tempVal = tempVal + data
+            if tempVal == CheckSum(tempLine):
+                state += 1
+                print('5.1')
+            else: state = 0
+        elif state == 7:
+            print('6')
+            if data[0] == 4:
+                print('fdfs')
+                
+        
+                
+                
+            
+get_serialInfo()        
+    
+OpenSerial()
 
 CloseSerial()        
         
-get_serialInfo()       
+ReadSerial()    
 
-     
+CheckSum(data)  
 
-        
-def ReceiveData (str data):
-        for i in range (0, len(data)):
-            
-def SendData
-            
-   
-res = None
-for i in range(0, len(data)):
-    if data[i] == 4:
-        res = i +1
-        break
-if res == None:
-    print('No end of transmission')
-else:
-    print ('End of transmission')
-
-if 4 in rawdata:print ('First line \n Second line')
+ReceiveData(data)
 
 
-#from ASCII values to characters
-data =[1, 32, 55, 2, 87, 69, 76, 67, 79, 77, 69, 3, 53, 65, 4, 1, 49, 52, 2, 90, 77, 101, 116, 101, 114, 52, 119, 32, 118, 32, 52, 46,
- 50, 32, 67, 79, 82, 66, 73, 3, 55, 57, 4]
-characters = [chr(ascii) for ascii in data]
-''.join(characters)
-
-#from characters to ASCII values
-s = '\x01 7\x02WELCOME\x035A\x04\x0114\x02ZMeter4w v 4.2 CORBI\x0379\x04'
-ASCII = [ord(c) for c in s]
 
 
-            
-def OpenSerial2():
-    ser = serial.Serial('COM3', baudrate = 115200, timeout = None)
-    if ser.is_open == False:
-        print('Not connected')
-        ser.close()
-    else:
-        print(ser.name +' Connected')
-        ser.open()
+
         
             
 
